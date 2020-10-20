801c4ff098f70575b3a7a225be4d5045e822c348
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-10-20 15:33:39 -0400

    ran build, needs styling

---
+++ frontend/static/build/precache-manifest.843335454fc5fcae9d1a22f0570cec7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0531c5a1961bf8475598976e185f2872",
+    "url": "/index.html"
+  },
+  {
+    "revision": "132b69d689cb9fce3c1f",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "c05a88e552303a6ed5da",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "132b69d689cb9fce3c1f",
+    "url": "/static/js/2.c12eb452.chunk.js"
+  },
+  {
+    "revision": "52e0a5fdb1bb48ae9a45ea794dab3cbc",
+    "url": "/static/js/2.c12eb452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c05a88e552303a6ed5da",
+    "url": "/static/js/main.58a1d740.chunk.js"
+  },
+  {
+    "revision": "61e07dcfa1eb4aac95f5",
+    "url": "/static/js/runtime-main.34b54e52.js"
+  }
+]);
--- frontend/static/build/service-worker.js
+++ frontend/static/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67f633c677263e33927277221c6ee0ed.js"
+  "/precache-manifest.843335454fc5fcae9d1a22f0570cec7a.js"
 );
 
 self.addEventListener('message', (event) => {
